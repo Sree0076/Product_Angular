a60d006263e9421715cf82e05539739b390cfa7a
Sreejith Shaji <sreejithshaji0076@gmail.com>
2024-06-20 12:03:06 +0530

    20-06

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,16 @@
+import { Component ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent {
+  @Input() category: any[] = [];
+  @Input() product: any[] = [];
+
+}
+
+
